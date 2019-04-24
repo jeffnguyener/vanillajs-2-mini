7f57275132bae19d41b1334fa8cdf845be53e752
Jeff Nguyen <jeffnguyener@Samanthas-MacBook-Pro.local>
2019-04-24 12:30:54 -0600

    Update todo

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ form.addEventListener('submit', addTodo);
 function addTodo(e) {
     e.preventDefault();
     let element = document.createElement('li')
+    element.addEventListener('click', completeTodo);
     let input = document.querySelector('#item').value;
     element.innerText = input;
     let button = document.createElement('button');
@@ -15,5 +16,16 @@ function addTodo(e) {
 }
 
 function removeTodo(e) {
+    console.log('event target \name', e.target);
+    console.log('parent node');
+    console.dir(e.target.parentNode);
     e.target.parentNode.remove();
+}
+
+function completeTodo(e) {
+    let checked = e.target.getAttribute('aria-checked')
+    if (checked !== 'true') {
+        e.target.setAttribute('aria-checked', 'true');
+    }
+    else e.target.setAttribute('aria-checked', 'false');
 }
